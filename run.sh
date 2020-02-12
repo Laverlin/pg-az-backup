4a955f24510484d3b5ddfcd4d1df59e25444f4e2
Laverlin <ilaverlin@gmail.com>
2020-02-12 15:16:20 -0800

    log on run

--- run.sh
+++ run.sh
@@ -3,7 +3,9 @@
 set -e
 
 if [ "${SCHEDULE}" = "**None**" ]; then
+    echo "run backup"
   bash backup.sh
 else
+    echo "schedule backup $SCHEDULE"
   exec go-cron "$SCHEDULE" /bin/bash backup.sh
 fi
