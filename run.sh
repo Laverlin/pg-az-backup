ec10c272d44479d1270a7991f50f3eec28a77123
IB <i6162746@hotmail.com>
2020-03-09 12:28:47 +0300

    fix schedule run path

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@ set -euo pipefail
 
 if [ "${SCHEDULE}" = "**None**" ]; then
     echo "run backup"
-    bash backup.sh
+    /bin/bash backup.sh
 else
     echo "schedule backup $SCHEDULE"
-    JOB="$SCHEDULE bash backup.sh > /proc/1/fd/1 2>/proc/1/fd/2"
+    JOB="$SCHEDULE /bin/bash /backup.sh > /proc/1/fd/1 2>/proc/1/fd/2"
     echo "$JOB" > /etc/crontabs/root
     crond -f -d 8 
 fi
